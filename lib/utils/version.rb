aa0257ce499d039d5ca1fd4a28d5f8a1ebf443d1
Nick Nicholas <opoudjis@gmail.com>
2022-08-16 23:33:37 +1000

    version bump

--- lib/utils/version.rb
+++ lib/utils/version.rb
@@ -1,5 +1,5 @@
 module Metanorma
   module Utils
-    VERSION = "1.2.9".freeze
+    VERSION = "1.3.0".freeze
   end
 end
---
+++ spec/xml_spec.rb
@@ -0,0 +1,69 @@
+require "spec_helper"
+require "fileutils"
+
+RSpec.describe Metanorma::Utils do
+  it "normalises anchors" do
+    expect(Metanorma::Utils.to_ncname("/:ab")).to eq "__ab"
+    expect(Metanorma::Utils.to_ncname("Löwe")).to eq "L__xf6_we"
+    expect(Metanorma::Utils.to_ncname("Löwe", asciionly: false)).to eq "Löwe"
+    expect(Metanorma::Utils.to_ncname("Löwe",
+                                      asciionly: true)).to eq "L__xf6_we"
+  end
+
+  it "sets metanorma IDs if not provided" do
+    uuid = /^_[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
+    expect(Metanorma::Utils.anchor_or_uuid)
+      .to match (uuid)
+    expect(Metanorma::Utils.anchor_or_uuid(Dummy.new(nil)))
+      .to match (uuid)
+    expect(Metanorma::Utils.anchor_or_uuid(Dummy.new("")))
+      .to match (uuid)
+    expect(Metanorma::Utils.anchor_or_uuid(Dummy.new("A"))).to eq "A"
+  end
+
+  it "generates XML snippets from Asciidoctor" do
+    input = <<~'EOS'
+      <A> &#150;
+    EOS
+
+    doc = (Asciidoctor::Document.new input.lines,
+                                     { standalone: false }).parse
+    out = Metanorma::Utils.noko do |xml|
+      xml << doc.blocks.first.content
+    end.join
+    expect(out).to be_equivalent_to <<~OUTPUT
+      &lt;A&gt; 
+    OUTPUT
+  end
+
+  it "wraps an Asciidoctor node in paragraph" do
+    input = <<~'EOS'
+      NOTE: XYZ
+    EOS
+    input2 = <<~'EOS'
+      ====
+      A
+
+      B
+      ====
+    EOS
+
+    doc = (Asciidoctor::Document.new input.lines,
+                                     { standalone: false }).parse
+    doc2 = (Asciidoctor::Document.new input2.lines,
+                                      { standalone: false }).parse
+
+    out = Metanorma::Utils.noko do |xml|
+      Metanorma::Utils.wrap_in_para(doc.blocks.first, xml)
+    end.join
+    expect(out).to be_equivalent_to <<~OUTPUT
+      <p>XYZ</p>
+    OUTPUT
+    out = Metanorma::Utils.noko do |xml|
+      Metanorma::Utils.wrap_in_para(doc2.blocks.first, xml)
+    end.join
+    expect(out).to be_equivalent_to <<~OUTPUT
+      <div class="paragraph"><p>A</p></div><div class="paragraph"><p>B</p></div>
+    OUTPUT
+  end
+end
