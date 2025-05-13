fb900dabd025cd09f746f8b066577168976d127f
Nick Nicholas <opoudjis@gmail.com>
2025-05-13 21:52:43 +1000

    extent CJK matching to CJK Script Extensions (e.g. pfull-width punctuation): https://github.com/metanorma/metanorma-standoc/issues/1017

--- lib/utils/cjk.rb
+++ lib/utils/cjk.rb
@@ -12,41 +12,41 @@ module Metanorma
       
       # CJK Symbols and Punctuation (U+3000–U+303F)
       # Used across all CJK scripts
-      CJK_SYMBOLS = "\\u3000-\\u303F".freeze
+      CJK_SYMBOLS = "[\\u3000-\\u303F]".freeze
       
       # CJK Punctuation (subset of CJK Symbols commonly used)
-      CJK_PUNCTUATION = "\\u3001-\\u3003\\u3008-\\u3011\\u3014-\\u301F".freeze
+      CJK_PUNCTUATION = "[\\u3001-\\u3003\\u3008-\\u3011\\u3014-\\u301F]".freeze
       
       # Halfwidth and Fullwidth Forms (U+FF00–U+FFEF)
       # Used in all CJK contexts
-      CJK_HALFWIDTH_FULLWIDTH = "\\uFF00-\\uFFEF".freeze
+      CJK_HALFWIDTH_FULLWIDTH = "[\\uFF00-\\uFFEF]".freeze
       
       # CJK Compatibility Forms (U+FE30–U+FE4F)
       # Primarily used with Han but relevant for all CJK
-      CJK_COMPAT = "\\uFE30-\\uFE4F".freeze
+      CJK_COMPAT = "[\\uFE30-\\uFE4F]".freeze
       
       # Vertical Forms (U+FE10–U+FE1F)
       # Used in vertical text layout for all CJK
-      CJK_VERTICAL = "\\uFE10-\\uFE1F".freeze
+      CJK_VERTICAL = "[\\uFE10-\\uFE1F]".freeze
       
       # Small Form Variants (U+FE50–U+FE6F)
       # Used in all CJK contexts
-      CJK_SMALL_FORMS = "\\uFE50-\\uFE6F".freeze
+      CJK_SMALL_FORMS = "[\\uFE50-\\uFE6F]".freeze
       
       # Ideographic Description Characters (U+2FF0–U+2FFF)
       # Used with Han script
-      HAN_IDC = "\\u2FF0-\\u2FFF".freeze
+      HAN_IDC = "[\\u2FF0-\\u2FFF]".freeze
       
       # Kanbun (U+3190–U+319F)
       # Used with Han script for Japanese
-      KANBUN = "\\u3190-\\u319F".freeze
+      KANBUN = "[\\u3190-\\u319F]".freeze
       
       # CJK Compatibility (U+3300–U+33FF)
       # Used with Han script
-      CJK_COMPAT_IDEOGRAPHS = "\\u3300-\\u33FF".freeze
+      CJK_COMPAT_IDEOGRAPHS = "[\\u3300-\\u33FF]".freeze
       
       # CJK Compatibility Ideographs (U+F900–U+FAFF)
-      HAN_COMPAT_IDEOGRAPHS = "\\uF900-\\uFAFF".freeze
+      HAN_COMPAT_IDEOGRAPHS = "[\\uF900-\\uFAFF]]".freeze
       
       # Script extensions by primary script
       HAN_EXTENSIONS = [
