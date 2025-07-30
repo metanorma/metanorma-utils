be55daf0c89925cb1aa5a439d212cbc9697031d3
Nick Nicholas <opoudjis@gmail.com>
2025-07-30 21:10:27 +1000

    replace Xml::C14n with Canon

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ end
 require "rspec/matchers"
 require "equivalent-xml"
 require "metanorma-utils"
-require "xml-c14n"
+require "canon"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
